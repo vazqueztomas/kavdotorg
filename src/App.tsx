d928b6cd9fcf1d85d169ad2877ce12ff2e422921
Tomas Vazquez <vazquezt2018@gmail.com>
2023-03-20 14:41:17 -0300

    added new hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import CategoriesPreview from "./components/CategoriesPreview"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Products from "./components/Products"
 
 function App() {
   return (
     <div>
-     <Navbar/>
+      <Hero/>
+     {/* <Navbar/> */}
      <Products/>
+     <CategoriesPreview/>
     </div>
   )
 }
